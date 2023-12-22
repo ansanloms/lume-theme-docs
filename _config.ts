3b1be0ce2d313bfe51b41fd8504ed8b4b0561444
ansanloms <ansanloms@users.noreply.github.com>
2023-12-22 22:48:02 +0900

    upgrade

--- _config.ts
+++ _config.ts
@@ -44,34 +44,38 @@ site.use(docs({
 site.ignore("README.md");
 site.remoteFile("docs/index.md", "README.md");
 
-site.preprocess([".html"], (page) => {
-  if (
-    typeof page.data.url === "string" && page.data.url.slice(0, 5) === "/docs"
-  ) {
-    page.data.url = page.data.url.slice(5);
+site.preprocess([".html"], (pages) => {
+  for (const page of pages) {
+    if (
+      typeof page.data.url === "string" && page.data.url.slice(0, 5) === "/docs"
+    ) {
+      page.data.url = page.data.url.slice(5);
+    }
   }
 });
 
-site.process([".html"], (page) => {
-  const { document } = page;
-  if (!document) {
-    return;
+site.process([".html"], (pages) => {
+  for (const page of pages) {
+    const { document } = page;
+    if (!document) {
+      continue;
+    }
+
+    const linkElement = document.createElement("link");
+    linkElement.setAttribute("rel", "stylesheet");
+    linkElement.setAttribute(
+      "href",
+      "https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-nord.min.css",
+    );
+    linkElement.setAttribute(
+      "integrity",
+      "sha512-/1nWQ0aAin0IGM5zDndLyY+6xUSiqA1ILh4Mm0XjSqqj4cXOH36rB/2Ep96sT4FOxvNEnUxyPNwqPlEmuImAFw==",
+    );
+    linkElement.setAttribute("crossorigin", "anonymous");
+    linkElement.setAttribute("referrerpolicy", "no-referrer");
+
+    document.head.append(linkElement);
   }
-
-  const linkElement = document.createElement("link");
-  linkElement.setAttribute("rel", "stylesheet");
-  linkElement.setAttribute(
-    "href",
-    "https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-nord.min.css",
-  );
-  linkElement.setAttribute(
-    "integrity",
-    "sha512-/1nWQ0aAin0IGM5zDndLyY+6xUSiqA1ILh4Mm0XjSqqj4cXOH36rB/2Ep96sT4FOxvNEnUxyPNwqPlEmuImAFw==",
-  );
-  linkElement.setAttribute("crossorigin", "anonymous");
-  linkElement.setAttribute("referrerpolicy", "no-referrer");
-
-  document.head.append(linkElement);
 });
 
 export default site;
--- deps/lume_markdown_plugins/title.ts
+++ deps/lume_markdown_plugins/title.ts
@@ -1,3 +1,3 @@
-import title from "https://deno.land/x/lume_markdown_plugins@v0.5.0/title.ts";
+import title from "https://deno.land/x/lume_markdown_plugins@v0.7.0/title.ts";
 
 export default title;
