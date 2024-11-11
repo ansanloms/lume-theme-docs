e12d9e5111400e43841c3a1375b3753e2906dbdd
ansanloms <ansanloms@users.noreply.github.com>
2024-11-11 14:21:05 +0900

    build: upgrade.

--- deps/lume_markdown_plugins/toc.ts
+++ deps/lume_markdown_plugins/toc.ts
@@ -1,4 +1,4 @@
-import toc from "https://deno.land/x/lume_markdown_plugins@v0.7.0/toc.ts";
-export type { Node } from "https://deno.land/x/lume_markdown_plugins@v0.7.0/toc/mod.ts";
+import toc from "https://deno.land/x/lume_markdown_plugins@v0.7.1/toc.ts";
+export type { Node } from "https://deno.land/x/lume_markdown_plugins@v0.7.1/toc/mod.ts";
 
 export default toc;
